2fa69bba3033131f9f16554d645068884764978a
Maël Gaonach <mael@gaonach.com>
2020-11-07 16:15:42 +0100

    fix run script

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ INPUT="$1"
 WORKDIR="${INPUT}_workdir"
 RES="/res/$(basename $INPUT).ply"
 
-python3 openMVS/MvgMvsPipeline.py "$INPUT" "$WORKDIR"
+python3 openMVS/MvgMvsPipeline.py "/datasets/$INPUT" "$WORKDIR"
 
 cp "${WORKDIR}/mvs/scene_dense_mesh_refine_texture.ply" "$RES"
 
